e84eeea801c5dd3a374a417e5afe8249593e4a65
ANH DUC <benbman22@gmail.com>
2019-07-14 23:42:16 +0700

    add entity framework code first for data

---
+++ ShopOnlineTTS.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineTTS.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private ShopOnlineTTSDbContext dbContext;
+
+        public ShopOnlineTTSDbContext Init()
+        {
+            return dbContext ?? (dbContext = new ShopOnlineTTSDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
