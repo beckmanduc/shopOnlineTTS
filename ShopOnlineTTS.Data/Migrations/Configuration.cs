56c68dd3205cf077d13d70714a507026aabffb8f
ANH DUC <benbman22@gmail.com>
2019-07-28 23:50:47 +0700

    List data productCategory

--- ShopOnlineTTS.Data/Migrations/Configuration.cs
+++ ShopOnlineTTS.Data/Migrations/Configuration.cs
@@ -1,9 +1,10 @@
-namespace ShopOnlineTTS.Data.Migrations
+﻿namespace ShopOnlineTTS.Data.Migrations
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Model.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -17,33 +18,51 @@ namespace ShopOnlineTTS.Data.Migrations
 
         protected override void Seed(ShopOnlineTTS.Data.ShopOnlineTTSDbContext context)
         {
+            CreateProductCategorySample(context);
             //  This method will be called after migrating to the latest version.
-            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ShopOnlineTTSDbContext()));
+            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ShopOnlineTTSDbContext()));
 
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ShopOnlineTTSDbContext()));
+            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ShopOnlineTTSDbContext()));
 
-            var user = new ApplicationUser()
+            //var user = new ApplicationUser()
+            //{
+            //    UserName = "anhduc",
+            //    Email = "levuanhduc1996@gmail.com",
+            //    EmailConfirmed = true,
+            //    BirthDay = DateTime.Now,
+            //    FullName = "Technology Education"
+
+            //};
+
+            //manager.Create(user, "123654$");
+
+            //if (!roleManager.Roles.Any())
+            //{
+            //    roleManager.Create(new IdentityRole { Name = "Admin" });
+            //    roleManager.Create(new IdentityRole { Name = "User" });
+            //}
+
+            //var adminUser = manager.FindByEmail("levuanhduc1996@gmail.com");
+
+            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
+        }
+
+        private void CreateProductCategorySample(ShopOnlineTTS.Data.ShopOnlineTTSDbContext context)
+        {
+            if(context.ProductCategories.Count() == 0)
             {
-                UserName = "anhduc",
-                Email = "levuanhduc1996@gmail.com",
-                EmailConfirmed = true,
-                BirthDay = DateTime.Now,
-                FullName = "Technology Education"
-
+                List<ProductCategory> listProductCategory = new List<ProductCategory>()
+            {
+                new ProductCategory() { Name = "Bút viết",Alias = "but-viet",Status =true },
+                new ProductCategory() { Name = "Sổ sách",Alias = "so-sach",Status =true },
+                new ProductCategory() { Name = "Hồ sơ",Alias = "ho-so",Status =true },
+                new ProductCategory() { Name = "Máy tính",Alias = "may-tinh",Status =true }
             };
-
-            manager.Create(user, "123654$");
-
-            if (!roleManager.Roles.Any())
-            {
-                roleManager.Create(new IdentityRole { Name = "Admin" });
-                roleManager.Create(new IdentityRole { Name = "User" });
+                context.ProductCategories.AddRange(listProductCategory);
+                context.SaveChanges();
             }
-
-            var adminUser = manager.FindByEmail("levuanhduc1996@gmail.com");
-
-            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
-
+           
         }
     }
 }
---
+++ ShopOnlineTTS.Web/Api/ProductCategoryController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using ShopOnlineTTS.Model.Models;
+using ShopOnlineTTS.Service;
+using ShopOnlineTTS.Web.Infrastructure.Core;
+using ShopOnlineTTS.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace ShopOnlineTTS.Web.Api
+{
+    [RoutePrefix("api/productcategory")]
+    public class ProductCategoryController : ApiControllerBase
+    {
+        IProductCategoryService _productCategoryService;
+        public ProductCategoryController(IErrorService errorService,IProductCategoryService productCategoryService) 
+            : base(errorService)
+        {
+            this._productCategoryService = productCategoryService;
+        }
+
+        [Route("getall")]
+        [HttpGet]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetAll();
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+    }
+}
