11db786691b1b3e44330eff191d9fdc62d3465d3
ANH DUC <benbman22@gmail.com>
2019-08-02 10:36:22 +0700

    update form and auto make seo title service

--- ShopOnlineTTS.Web/Api/ProductCategoryController.cs
+++ ShopOnlineTTS.Web/Api/ProductCategoryController.cs
@@ -17,12 +17,15 @@ namespace ShopOnlineTTS.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
+        #region Initialize
+        private IProductCategoryService _productCategoryService;
         public ProductCategoryController(IErrorService errorService,IProductCategoryService productCategoryService) 
             : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+
+        #endregion
 
         [Route("getallparents")]
         [HttpGet]
@@ -38,6 +41,23 @@ namespace ShopOnlineTTS.Web.Api
                 return response;
             });
         }
+
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                return response;
+            });
+        }
+
 
         [Route("getall")]
         [HttpGet]
@@ -92,5 +112,35 @@ namespace ShopOnlineTTS.Web.Api
                 return response;
             });
         }
+
+        [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductCategoryViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(productCategoryVm.ID);
+                    dbProductCategory.UpdateProductCategory(productCategoryVm);
+                    dbProductCategory.UpdatedDate = DateTime.Now;
+
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
     }
 }
